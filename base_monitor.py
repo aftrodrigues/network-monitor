0185c6e812384aef38bd234a08870bf222251f9d
Andrei Filipe Teixeira Rodrigues <andreifilipe.t@hotmail.com>
2019-01-28 16:03:46 -0200

    Base Class to create the monitors with thread

---
+++ base_monitor.py
@@ -0,0 +1,18 @@
+class base_monitor(object):
+	"""
+	Abstract class to implemnt differents methods of monitoring the network
+	"""
+	def init(self):
+		self.process = None		
+
+	def start_monitor():
+		pass
+
+	def stop_monitor():
+		pass
+
+	def set_monitor():
+		pass
+
+	def results():
+		pass
